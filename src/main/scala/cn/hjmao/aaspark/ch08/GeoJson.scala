fa5f9bf7d1b61a105559262774af944a7f36cac5
Huajian Mao <huajianmao@gmail.com>
2017-03-21 10:32:47 +0800

    [WIP] Add geometry json processing helper functions

--- src/main/scala/cn/hjmao/aaspark/ch08/GeoJson.scala
+++ src/main/scala/cn/hjmao/aaspark/ch08/GeoJson.scala
@@ -1,6 +1,7 @@
 package cn.hjmao.aaspark.ch08
 
-import spray.json.JsValue
+import com.esri.core.geometry.{Geometry, GeometryEngine}
+import spray.json._
 
 /**
  * Created by hjmao on 17-3-20.
@@ -18,5 +19,66 @@ case class FeatureCollection(features: Array[Feature]) extends IndexedSeq[Featur
   def length: Int = features.length
 }
 
-class GeoJson {
+case class GeometryCollection(geometries: Array[RichGeometry]) extends IndexedSeq[RichGeometry] {
+  def apply(index: Int): RichGeometry = geometries(index)
+  def length: Int = geometries.length
+}
+
+object GeoJsonProtocol extends DefaultJsonProtocol {
+
+  implicit object RichGeometryJsonFormat extends RootJsonFormat[RichGeometry] {
+    def write(rg: RichGeometry): JsValue = {
+      GeometryEngine.geometryToGeoJson(rg.spatialReference, rg.geometry).parseJson
+    }
+
+    def read(value: JsValue): RichGeometry = {
+      val mg = GeometryEngine.geometryFromGeoJson(value.compactPrint, 0, Geometry.Type.Unknown)
+      new RichGeometry(mg.getGeometry, mg.getSpatialReference)
+    }
+  }
+
+  implicit object FeatureJsonFormat extends RootJsonFormat[Feature] {
+    def write(f: Feature): JsObject = {
+      val buf = scala.collection.mutable.ArrayBuffer(
+        "type" -> JsString("Feature"),
+        "properties" -> JsObject(f.properties),
+        "geometry" -> f.geometry.toJson
+      )
+      f.id.foreach(v => {
+        buf += "id" -> v
+      })
+      JsObject(buf.toMap)
+    }
+
+    def read(value: JsValue): Feature = {
+      val jso = value.asJsObject
+      val id = jso.fields.get("id")
+      val properties = jso.fields("properties").asJsObject.fields
+      val geometry = jso.fields("geometry").convertTo[RichGeometry]
+      Feature(id, properties, geometry)
+    }
+  }
+
+  implicit object FeatureCollectionJsonFormat extends RootJsonFormat[FeatureCollection] {
+    def write(fc: FeatureCollection): JsObject = {
+      JsObject(
+        "type" -> JsString("FeatureCollection"),
+        "features" -> JsArray(fc.features.map(_.toJson): _*)
+      )
+    }
+    def read(value: JsValue): FeatureCollection = {
+      FeatureCollection(value.asJsObject.fields("features").convertTo[Array[Feature]])
+    }
+  }
+  implicit object GeometryCollectionJsonFomat extends RootJsonFormat[GeometryCollection] {
+    def write(gc: GeometryCollection): JsObject = {
+      JsObject(
+        "type" -> JsString("GeometryCollection"),
+        "geometries" -> JsArray(gc.geometries.map(_.toJson): _*)
+      )
+    }
+    def read(value: JsValue): GeometryCollection = {
+      GeometryCollection(value.asJsObject.fields("geometries").convertTo[Array[RichGeometry]])
+    }
+  }
 }
--- src/main/scala/cn/hjmao/aaspark/ch08/RunGeoTime.scala
+++ src/main/scala/cn/hjmao/aaspark/ch08/RunGeoTime.scala
@@ -1,10 +1,84 @@
 package cn.hjmao.aaspark.ch08
 
+import java.text.SimpleDateFormat
+import java.util.concurrent.TimeUnit
+
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.functions._
+
 /**
  * Created by hjmao on 17-3-20.
  */
-object RunGeoTime {
-  def main(args: Array[String]): Unit = {
+class RichRow(row: Row) {
+  def getAs[T](field: String): Option[T] =
+    if (row.isNullAt(row.fieldIndex(field))) None
+    else Some(row.getAs[T](field))
+}
 
+case class Trip(
+  license: String,
+  pickupTime: Long,
+  dropoffTime: Long,
+  pickupX: Double,
+  pickupY: Double,
+  dropoffX: Double,
+  dropoffY: Double
+)
+
+object RunGeoTime {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().master("local[*]")
+                                      .appName("GeoTime in Ch08")
+                                      .getOrCreate()
+    import spark.implicits._
+
+    val taxiRaw = spark.read.option("header", "true").csv("data/ch08/trip_data*.csv")
+    val taxiParsed = taxiRaw.rdd.map(safe(parse))
+    val taxiGood = taxiParsed.map(_.left.get).toDS
+    taxiGood.cache()
+
+    val hours = (pickup: Long, dropoff: Long) => {
+      TimeUnit.HOURS.convert(dropoff - pickup, TimeUnit.MILLISECONDS)
+    }
+    val hoursDF = udf(hours)
+    taxiGood.groupBy(hoursDF($"pickupTime", $"dropoffTime").as("h")).count().sort("h").show()
+
+    spark.close()
+  }
+
+  def safe[S, T](f: S => T): S => Either[T, (S, Exception)] = {
+    new Function[S, Either[T, (S, Exception)]] with Serializable {
+      override def apply(s: S): Either[T, (S, Exception)] = {
+        try {
+          Left(f(s))
+        } catch {
+          case e: Exception => Right((s, e))
+        }
+      }
+    }
+  }
+
+  def parseTaxiTime(rr: RichRow, timeField: String): Long = {
+    val formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val optDt = rr.getAs[String](timeField)
+    optDt.map(formatter.parse(_).getTime).getOrElse(0L)
+  }
+
+  def parseTaxiLoc(rr: RichRow, locField: String): Double = {
+    rr.getAs[String](locField).map(_.toDouble).getOrElse(0.0)
+  }
+
+  def parse(line: Row): Trip = {
+    val rr = new RichRow(line)
+    Trip(
+      license = rr.getAs[String]("hack_license").orNull,
+      pickupTime = parseTaxiTime(rr, "pickup_datetime"),
+      dropoffTime = parseTaxiTime(rr, "dropoff_datetime"),
+      pickupX = parseTaxiLoc(rr, "pickup_longitude"),
+      pickupY = parseTaxiLoc(rr, "pickup_latitude"),
+      dropoffX = parseTaxiLoc(rr, "dropoff_longitude"),
+      dropoffY = parseTaxiLoc(rr, "dropoff_latitude")
+    )
   }
 }
